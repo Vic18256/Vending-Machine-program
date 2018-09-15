4d30617fa1d184dd907a921337b0702c3cd82313
Vic18256 <vherkan@hotmail.co.uk>
2018-09-15 19:27:00 +0100

    Create a test that will pass for all values
    instead of returning 1  change to it to amount so that passing all values will pass the test

--- vending_machine.py
+++ vending_machine.py
@@ -4,10 +4,16 @@ def get_change(amount):
     if amount == 0:
         return []
         
-    return [1]
+    return [amount]
     
     
 tests_are_equal(get_change(0), [])
 tests_are_equal(get_change(1), [1])
+tests_are_equal(get_change(2), [2])
+tests_are_equal(get_change(5), [5])
+tests_are_equal(get_change(10), [10])
+tests_are_equal(get_change(20), [20])
+tests_are_equal(get_change(50), [50])
+tests_are_equal(get_change(100), [100])
 
 print("All tests passed!")
