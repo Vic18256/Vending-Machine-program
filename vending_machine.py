19f521151291e72a86be49f9f7b355994c6f9377
Vic18256 <vherkan@hotmail.co.uk>
2018-09-15 19:11:35 +0100

    implement a test for single value
    We add an if statement to set the function to return nothing if amount is zero . However this test will return 1 for any values other than zero

--- vending_machine.py
+++ vending_machine.py
@@ -1,8 +1,13 @@
 from byotest import *
 
 def get_change(amount):
-    return []
+    if amount == 0:
+        return []
+        
+    return [1]
+    
     
 tests_are_equal(get_change(0), [])
+tests_are_equal(get_change(1), [1])
 
 print("All tests passed!")
