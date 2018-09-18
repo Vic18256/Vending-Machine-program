59c34a3d4d96e9aa611660f81916a5ff10673a55
Vic18256 <vherkan@hotmail.co.uk>
2018-09-18 21:05:24 +0100

    create a test that will return multiple coin values

--- vending_machine.py
+++ vending_machine.py
@@ -4,7 +4,11 @@ def get_change(amount):
     if amount == 0:
         return []
         
-    return [amount]
+    if amount in [100, 50, 20, 10, 5, 2, 1]:
+      return [amount]
+      
+      
+    return [2,1]
     
     
 tests_are_equal(get_change(0), [])
@@ -15,5 +19,6 @@ tests_are_equal(get_change(10), [10])
 tests_are_equal(get_change(20), [20])
 tests_are_equal(get_change(50), [50])
 tests_are_equal(get_change(100), [100])
+tests_are_equal(get_change(3), [2, 1])
 
 print("All tests passed!")
